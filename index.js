791c24b2336f24be44d61b495ebceb00c983bb70
Spencer Krull <spencerkrull138@gmail.com>
2022-04-13 19:16:17 -0600

    created for loops for bonus

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function tile(url, left, bottom, width, height) {
+    for(let h = 0; h < height; h++) {
+        for(let w = 0; w < width; w++) {
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
 function newImage(url, left, bottom) {
     let object = document.createElement('img')
     object.src = url
